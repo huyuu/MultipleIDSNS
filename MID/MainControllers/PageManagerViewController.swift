a6f2aa8523deb287d61639c537cca8d9b6be171f
江宇揚 <yuyang@kousakaiage-no-MacBook.local>
2018-12-10 23:44:13 +0900

    First commit

---
+++ MID/MainControllers/PageManagerViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PageManagerViewController.swift
+//  MID
+//
+//  Created by 江宇揚 on 2018/12/10.
+//  Copyright © 2018 Jiang Yuyang. All rights reserved.
+//
+
+import UIKit
+
+class PageManagerViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("view did load.")
+        
+        if let viewController = initiatePage(){
+            setViewControllers([viewController], direction: .forward, animated: false, completion: nil)
+            print("page exists.")
+        }
+    }
+    
+    
+    func initiatePage() -> UITableViewController? {
+        guard let page = storyboard!.instantiateViewController(withIdentifier: "MainViewController") as? UITableViewController else {   return nil }
+        
+        // page exits:
+        return page
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MID/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MID
+//
+//  Created by 江宇揚 on 2018/12/10.
+//  Copyright © 2018 Jiang Yuyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
